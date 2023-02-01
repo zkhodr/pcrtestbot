70a7d8212c5da98e2fa475551898b7cec9dd0375
Rich Hildred <rhildred@gmail.com>
2023-02-01 12:37:44 -0500

    simplify state machine.

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -1,40 +1,39 @@
-const OrderState = Object.freeze({
-    WELCOMING:   Symbol("welcoming"),
-    RESERVING:   Symbol("reserving")
-});
-
-class RapidTestOrder{
-    constructor(sFrom){
-        this.stateCur = OrderState.WELCOMING;
-        this.isDone = false;
-        this.sFrom = sFrom;
-    }
-    handleInput(sInput){
+class RapidTestOrder {
+  constructor(sFrom) {
+    this.OrderState = {
+      WELCOMING: () => {
         let aReturn = [];
-        switch(this.stateCur){
-            case OrderState.WELCOMING:
-                this.stateCur = OrderState.RESERVING;
-                aReturn.push("Welcome to Rich's Rapid Test.");
-                aReturn.push("Would you like to reserve a rapid test kit?");
-                break;
-            case OrderState.RESERVING:
-                this.isDone = true;
-                if(sInput.toLowerCase().startsWith('y')){
-                  aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-                  let d = new Date();
-                  d.setMinutes(d.getMinutes() + 120);
-                  aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
-                }else{
-                  aReturn.push("Thanks for trying our reservation system");
-                  aReturn.push("Maybe next time")
-                }
-                break;
+        this.stateCur = this.OrderState.RESERVING;
+        aReturn.push("Welcome to Rich's Rapid Test.");
+        aReturn.push("Would you like to reserve a rapid test kit?");
+        return aReturn;
+      },
+      RESERVING: (sInput) => {
+        let aReturn = [];
+        this.isDone = true;
+        if (sInput.toLowerCase().startsWith('y')) {
+          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          let d = new Date();
+          d.setMinutes(d.getMinutes() + 120);
+          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+        } else {
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time")
         }
         return aReturn;
-    }
-    isDone(){
-      return this.isDone;
-    }
+      }
+    };
+
+    this.stateCur = this.OrderState.WELCOMING;
+    this.isDone = false;
+    this.sFrom = sFrom;
+  }
+  handleInput(sInput) {
+    return this.stateCur(sInput);
+  }
+  isDone() {
+    return this.isDone;
+  }
 }
 
-export {RapidTestOrder}
+export { RapidTestOrder }
--- spec/RapidTestOrderSpec.js
+++ spec/RapidTestOrderSpec.js
@@ -12,7 +12,7 @@ describe("order test", function(){
         const aResult = oOrder.handleInput("yes");
         expect(true).toEqual(aResult[0].toLowerCase().includes("reserved"));
     });
-    it("tests results of new", function(){
+    it("tests results of no", function(){
         const oOrder = new RapidTestOrder("519-123-4567");
         oOrder.handleInput("hello");
         const aResult = oOrder.handleInput("no");
