a88e37c39bab981c5502db3095fbfe4cce17c71a
zkhodr <khodrzaky@gmail.com>
2024-02-15 20:49:43 -0500

    RedLobsterOrder

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -1,39 +1,39 @@
 class RapidTestOrder {
-  constructor(sFrom) {
-    this.OrderState = {
-      WELCOMING: () => {
-        let aReturn = [];
-        this.stateCur = this.OrderState.RESERVING;
-        aReturn.push("Welcome to Rich's Acton Rapid Test.");
-        aReturn.push("Would you like to reserve a rapid test kit?");
-        return aReturn;
-      },
-      RESERVING: (sInput) => {
-        let aReturn = [];
-        this.isDone = true;
-        if (sInput.toLowerCase().startsWith('y')) {
-          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-          let d = new Date();
-          d.setMinutes(d.getMinutes() + 120);
-          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
-        } else {
-          aReturn.push("Thanks for trying our reservation system");
-          aReturn.push("Maybe next time")
-        }
-        return aReturn;
-      }
-    };
+    constructor(sFrom) {
+        this.OrderState = {
+            WELCOMING: () => {
+                let aReturn = [];
+                this.stateCur = this.OrderState.RESERVING;
+                aReturn.push("Welcome to Red Lobster.");
+                aReturn.push("May I take your order?");
+                return aReturn;
+            },
+            RESERVING: (sInput) => {
+                let aReturn = [];
+                this.isDone = true;
+                if (sInput.toLowerCase().startsWith('y')) {
+                    aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+                    let d = new Date();
+                    d.setMinutes(d.getMinutes() + 120);
+                    aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+                } else {
+                    aReturn.push("Thanks for trying our reservation system");
+                    aReturn.push("Maybe next time")
+                }
+                return aReturn;
+            }
+        };
 
-    this.stateCur = this.OrderState.WELCOMING;
-    this.isDone = false;
-    this.sFrom = sFrom;
-  }
-  handleInput(sInput) {
-    return this.stateCur(sInput);
-  }
-  isDone() {
-    return this.isDone;
-  }
+        this.stateCur = this.OrderState.WELCOMING;
+        this.isDone = false;
+        this.sFrom = sFrom;
+    }
+    handleInput(sInput) {
+        return this.stateCur(sInput);
+    }
+    isDone() {
+        return this.isDone;
+    }
 }
 
 export { RapidTestOrder }
