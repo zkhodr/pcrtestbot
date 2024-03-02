ff354eae67873fcddee20578454b63436b496fc8
zkhodr <khodrzaky@gmail.com>
2024-03-01 22:47:00 -0500

    RedLobster

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -1,58 +1,91 @@
-class RapidTestOrder {
-    constructor(sFrom) {
-        this.OrderState = {
-            WELCOMING: () => {
-                let aReturn = [];
-                this.stateCur = this.OrderState.ORDERING;
+const OrderState = Object.freeze({
+    WELCOMING: Symbol("welcoming"),
+    ORDERING: Symbol("ordering"),
+    SALMON_ORLEANS: Symbol("salmon orleans"),
+    FIRE_GRILLED_SALMON: Symbol("fire grilled salmon"),
+    TOPPINGS: Symbol("toppings"),
+    SIZE: Symbol("size"),
+    DRINKS: Symbol("drinks"),
+    PRICE: Symbol("price"),
+    DESSERTS: Symbol("desserts")
+
+});
+
+class RapidTestOrder{
+    constructor(sFrom){
+        this.stateCur = OrderState.WELCOMING;
+        this.isDone = false;
+        this.sFrom = sFrom;
+        this.main = ""; 
+        this.toppings = "";
+        this.drink = ""; 
+        this.dessert = "";  
+        
+    }
+    
+    handleInput(sInput){
+        let aReturn = [];
+
+        
+        
+        switch(this.stateCur){
+            case OrderState.WELCOMING:
+                this.stateCur = OrderState.ORDERING;
                 aReturn.push("Welcome to Red Lobster.");
                 aReturn.push("May I take your order?");
-                return aReturn;
-            },
-              ORDERING: (sInput) => {
-                let aReturn = [];
-                this.stateCur = this.OrderState.ORDERING;
+                
+                break;
+
+            case OrderState.ORDERING:
+                this.main = String(sInput); 
+                this.stateCur = OrderState.TOPPINGS;
                 if(sInput.toLowerCase().includes('salmon') ||sInput.toLowerCase().includes('orleans') || sInput.toLowerCase().includes('grilled')){
                   aReturn.push("sounds delicious :) what size of the Salmon would you like to order?");
                   
                 }
-                return aReturn;
-            },
-                ORDERING: (sInput) => {
-                let aReturn = [];
-                this.stateCur = this.OrderState.ORDERING;
+                break;
+            case OrderState.TOPPINGS: 
                 aReturn.push("What toppings would you like with that? choose from the following: shrimp tossed in a Cajun butter; Tiryaki Sauce");
                 this.stateCur = OrderState.DRINKS;
-                }
-                return aReturn;
-            },
-            RESERVING: (sInput) => {
-                let aReturn = [];
-                this.isDone = true;
-                if (sInput.toLowerCase().startsWith('y')) {
-                    aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-                    let d = new Date();
-                    d.setMinutes(d.getMinutes() + 120);
-                    aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
-                } else {
-                    aReturn.push("Thanks for trying our reservation system");
-                    aReturn.push("Maybe next time")
-                }
-                return aReturn;
-            }
-
+                
+   
+                break;
             
-        };
+            case OrderState.DRINKS:
+                this.toppings = String(sInput); 
+                aReturn.push("Would you like a drink with that?");
+                aReturn.push("Choose from: Pepsi, Cola, Orange Juice, Apple Juice");
+                this.stateCur = OrderState.DESSERTS;
 
-        this.stateCur = this.OrderState.WELCOMING;
-        this.isDone = false;
-        this.sFrom = sFrom;
+                break;
+
+            case OrderState.DESSERTS:
+                this.drink = String(sInput);
+                aReturn.push("Would you like a desserts with that?");
+                aReturn.push("Choose from: Vanilla Bean Cheesecake, Chocolate Wave, Brownie Overboard, Warm Apple Crostade");
+                this.stateCur = OrderState.PRICE;
+
+                break;
+
+            case OrderState.PRICE: 
+                this.dessert = String(sInput);
+                aReturn.push("Great Choice! Your total is: $28.78");
+                aReturn.push("You order the following: "); 
+                aReturn.push(this.main); 
+                aReturn.push(this.toppings);
+                aReturn.push(this.drink);  
+                aReturn.push(this.dessert); 
+                
+                break;
+                
+            }
+             
+
+        return aReturn;
     }
-    handleInput(sInput) {
-        return this.stateCur(sInput);
-    }
-    isDone() {
-        return this.isDone;
+    isDone(){
+      return this.isDone;
     }
 }
 
-export { RapidTestOrder }
+export{RapidTestOrder}
--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.post("/sms", (req, res) => {
   }
 });
 
-const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${server.address().port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
